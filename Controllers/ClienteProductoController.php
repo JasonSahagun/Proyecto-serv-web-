aba4e2b9afc95ac15d6aecca79b4fa3388e3d069
JasonSahagun <jason.sahagun0046@alumnos.udg.mx>
2025-12-01 19:15:26 -0600

    ClienteProductoController.php

--- Controllers/ClienteProductoController.php
+++ Controllers/ClienteProductoController.php
@@ -8,13 +8,12 @@ use Illuminate\Support\Facades\DB;
 class ClienteProductoController extends Controller
 {
     //
-    public function catalogo(){
+    public function catalogo(){ //De aqui solo toma los productos con imagenes y los mostramos 
         $products=DB::table('products')->where('status','ACTIVO')->get();
-        //return view ('/productos/catalogo')->with ('productos',$products);
         return view ('/productos/products')->with ('productos',$products);
     }
 
-    public function detalle($id){
+    public function detalle($id){ //esta es la vista detallada de productos 
         $products=DB::table('products')->where('id', $id)->where('status','ACTIVO')->get();
         return view('/productos/detalle')->with ('product',$products);
 
